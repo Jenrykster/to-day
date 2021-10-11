2cbadd71bece17ba63658cb7c3b2665d0a621e9d
João Henrique <joaoenrique.san@gmail.com>
2021-10-11 15:12:15 -0300

    Add functions to render tasks and projects to domManager

--- src/Modules/domManager.js
+++ src/Modules/domManager.js
@@ -1,15 +1,34 @@
 const domManager = (function(){
     let sideBar;
-    const init = (domElement, projects) => {
+    let taskList;
+    const render = (domElement, projects) => {
+        domElement.innerHTML = ''; //Clean the page contents 
+
         sideBar = document.createElement('div');
         sideBar.classList.add('side-bar');
-        
         addProjectsToSidebar(projects);
+
+        taskList = document.createElement('div');
+        taskList.classList.add('task-list');
+        addTasklistContents(projects);
+        
         domElement.appendChild(sideBar);
+        domElement.appendChild(taskList);
     }
     const createProjectElement = (project) => {
         let newProjectElement = document.createElement('div');
         newProjectElement.innerHTML = `<div class="project"><h2>${project.name}</h3></div>`;
+        return newProjectElement;
+    }
+    const createTaskElement = (task) => {
+        let newProjectElement = document.createElement('div');
+        newProjectElement.innerHTML = `<div class="task">
+        <div class="task-header">
+                <h2>${task.title}</h2>
+                <h3>${task.dueDate}</h3>
+            </div>
+            <p>${task.description}</p>
+        </div>`;
         return newProjectElement;
     }
     const addProjectsToSidebar = (projects) => {
@@ -18,8 +37,17 @@ const domManager = (function(){
             sideBar.appendChild(newProject);
         })
     }
+    const addTasklistContents = (projects) => {
+        let title = document.createElement('h1');
+        title.innerHTML = projects[0].name;
+        taskList.appendChild(title);
+        projects[0].tasks.forEach(task =>{
+            let newTask = createTaskElement(task);
+            taskList.appendChild(newTask);
+        })
+    }
     return {
-        init
+        render
     } 
 })();
 
